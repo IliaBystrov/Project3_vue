b956f4357d45cbd2e9fcd069e1377f67316638b6
Ilia <32134793+IliaBystrov@users.noreply.github.com>
2024-07-24 17:26:23 +0300

    up1

--- Project3_vue/src/App.vue
+++ Project3_vue/src/App.vue
@@ -1,8 +1,28 @@
 <template>
+  <header_comp></header_comp>
 </template>
 
 <script>
-  import axios from 'axios';
+import Card from './components/card.vue';
+import Footer_comp from './components/footer_comp.vue';
+import Header_comp from './components/header_comp.vue';
+
+  export default{
+    components: {Card, Footer_comp, Header_comp},
+    data(){
+      return{
+        data_list: [],
+        cards: [],
+
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+  } 
 </script>
 
 <style scoped>
---
+++ Project3_vue/src/components/header_comp.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="header_comp">
+        <img src="" class="logo">
+        <h1>Кино-Сериалы-Мультики</h1>
+    </div>
+</template>
+
+<script>
+  export default{
+    data(){
+
+    },
+    computed: {
+
+    },
+    methods: {
+      
+    }
+  } 
+</script>
+
+<style scoped>
+.header_comp{
+    width: 100%;
+    height: 120px;
+    background: rgb(255,220,130);
+    background: linear-gradient(120deg, rgba(255,220,130,1) 0%, rgba(255,100,100,1) 100%);
+    display: flex;
+    flex-wrap: wrap;
+    box-shadow: 0px 2px 5px rgba(255, 212, 128, 1);
+}
+img{
+    width: 90px;
+    height: 90px;
+}
+h1{
+    background-image: linear-gradient(120deg, rgba(204,0,255,1) 0%, rgba(101,77,255,1) 100%);
+    color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;
+    font-size: 32px;
+}
+</style>
